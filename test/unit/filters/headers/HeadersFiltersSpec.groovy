4b46e29766f28351818c964ea5efb368fa9cd63c
Asiel <lealceldeiro@gmail.com>
2017-06-21 10:09:32 -0400

    Add test for header filter

---
+++ test/unit/filters/headers/HeadersFiltersSpec.groovy
@@ -0,0 +1,17 @@
+package filters.headers
+
+import grails.test.mixin.Mock
+import spock.lang.Specification
+
+@Mock(HeadersFilters)
+class HeadersFiltersSpec extends Specification {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+    }
+}
